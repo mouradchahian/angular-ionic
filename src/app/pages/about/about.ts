345e46b019af53a582eff844f4f02a9902b29de0
unknown <mouradchahiane@gmail.com>
2023-05-23 11:50:21 +0200

    demo release v1.0

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { Platform, PopoverController } from '@ionic/angular';
 
-import { PopoverPage } from '../about-popover/about-popover';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'page-about',
@@ -10,24 +9,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['./about.scss'],
 })
 export class AboutPage {
-  location = 'madison';
-  conferenceDate = '2047-05-17';
 
-  selectOptions = {
-    header: 'Select a Location'
-  };
 
   constructor(public popoverCtrl: PopoverController, private platform: Platform, private statusBar: StatusBar) {
     this.initializeApp();
    }
-
-  async presentPopover(event: Event) {
-    const popover = await this.popoverCtrl.create({
-      component: PopoverPage,
-      event
-    });
-    await popover.present();
-  }
 
   async ngOnInit() {
     this.initializeApp();
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  authorize: 'AAAAqwBNQjc:APA91bGGWx-Zw6-rDew7mUHuyxZy6c8tdhYQ0kJ7PLVmKokGxSiFZLev9Ec3oHv1mtXO3XFeV9TRgRq32e5NNseW85B62-phs4qxoNFdT0M5DuTXYQs6pRUry6VhQvSeJwNxB4NLypsu',
+  fcmUrl:'https://fcm.googleapis.com/fcm/send',
+  url:'http://192.168.1.10:3000'
 };
